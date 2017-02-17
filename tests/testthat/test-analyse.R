02c258c4b2719f2ea2bdaea8b0174409220db839
joethorley <joe@poissonconsulting.ca>
2017-02-17 09:07:11 -0800

    quick predict fixed

--- tests/testthat/test-analyse.R
+++ tests/testthat/test-analyse.R
@@ -89,6 +89,6 @@ test_that("analyse", {
   expect_identical(colnames(year), c("Site", "HabitatQuality", "Year", "Visit",
                                         "Density", "YearFactor",
                                      "estimate", "lower", "upper"))
-  expect_true(all(is.na(year$lower)))
+  expect_true(all(year$lower < year$estimate))
   expect_false(is.unsorted(year$estimate))
 })
