f8798c1ed95fa3d9c70b41037595101fb3a8c2b4
joethorley <joe@poissonconsulting.ca>
2017-02-20 15:35:15 -0800

    adjust predict now has sd etc

--- tests/testthat/test-analyse.R
+++ tests/testthat/test-analyse.R
@@ -88,7 +88,7 @@ test_that("analyse", {
   expect_is(year, "tbl")
   expect_identical(colnames(year), c("Site", "HabitatQuality", "Year", "Visit",
                                         "Density", "YearFactor",
-                                     "estimate", "lower", "upper"))
+                                     "estimate", "sd", "zscore", "lower", "upper", "pvalue"))
   expect_true(all(year$lower < year$estimate))
   expect_false(is.unsorted(year$estimate))
 })
