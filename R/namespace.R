cd334b7b8bb8e0066223ef0753e4ece179d76e0c
joethorley <joe@poissonconsulting.ca>
2020-03-01 13:20:13 -0800

    removing dependencies

--- R/namespace.R
+++ R/namespace.R
@@ -1,5 +1,4 @@
 #' @import chk mcmcr mbr stringr universals extras term nlist
-#' @importFrom broom glance
 #' @importFrom lubridate dminutes dhours is.duration
 #' @importFrom plyr llply
 #' @importFrom stats coef predict update
